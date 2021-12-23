f51c3c00ea8f6b8b402d7e2d7cdee60d7161635f
unknown <olasunkanmizik@gmail.com>
2021-12-23 18:23:20 +0100

    latest development

---
+++ src/main/java/laundry/laundry/route/LaundryRoute.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laundry.laundry.route;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author HP
+ */
+@RestController
+@RequestMapping("/product")
+public class LaundryRoute {
+
+    public ResponseEntity greeting(){
+        return ResponseEntity.ok("Welcome");
+    }
+
+}
